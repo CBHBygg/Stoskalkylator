2d280ee57fad615833b295f08da384afa04eb633
CBHBygg <christopher@cbhbygg.se>
2025-09-21 17:57:49 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,27 @@
-const CACHE_NAME = "stoskalk-v7";./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./vendor/jspdf.umd.min.js",
-  "./vendor/svg2pdf.umd.min.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const CACHE_NAME = "stoskalk-v7";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/style.css",
+  "/vendor/jspdf.umd.min.js",
+  "/vendor/svg2pdf.umd.min.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
   );
   self.clients.claim();
 });
